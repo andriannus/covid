6c8a4f5269fb5fb1bffc831d76350d562c86e0bb
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-06 19:27:26 +0700

    chore: use amar-ui on app

--- src/app/app.component.vue
+++ src/app/app.component.vue
@@ -3,4 +3,4 @@
 </template>
 
 <script lang="ts" src="./app.component.ts"></script>
-<style lang="scss" scoped src="./app.component.scss"></style>
+<style lang="scss" src="./app.component.scss"></style>
