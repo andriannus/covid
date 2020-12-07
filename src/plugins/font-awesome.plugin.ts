8416c60bfc855e87ec40344821bd15cc7e346b4e
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-07 19:38:15 +0700

    refactor: add new icon

--- src/plugins/font-awesome.plugin.ts
+++ src/plugins/font-awesome.plugin.ts
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+  faExclamation,
+  faHome,
+  faInfoCircle,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faHome, faInfoCircle);
+library.add(faExclamation, faHome, faInfoCircle);
 
 Vue.component('app-icon', FontAwesomeIcon);
