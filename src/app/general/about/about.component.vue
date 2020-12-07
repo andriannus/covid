7468f69378412b40a2fbafca31723cec41a4245a
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-07 19:25:46 +0700

    feat: create about page

---
+++ src/app/general/about/about.component.vue
@@ -0,0 +1,49 @@
+<template>
+  <app-content :has-toolbar="false">
+    <app-card>
+      <h1 class="FontSize-large FontWeight-medium">Tentang aplikasi ini</h1>
+
+      <div class="MarginTop TextAlign-justify">
+        <p class="FontWeight-semibold">Sumber data</p>
+        <span>
+          Data berasal dari API yang dikembangkan oleh
+
+          <a
+            href="http://https://github.com/mathdroid/covid-19-api"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            mathdroid
+          </a>
+        </span>
+      </div>
+
+      <div class="MarginTop TextAlign-justify">
+        <p class="FontWeight-semibold">Icon pada aplikasi</p>
+        <span>
+          Icon pada aplikasi baik itu berupa PNG atau SVG, berasal dari
+
+          <a
+            href="https://www.flaticon.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Flaticon
+          </a>
+        </span>
+      </div>
+    </app-card>
+
+    <template #contentFooter>
+      <router-link
+        class="Button Button--primary Button--outlined Button--fullWidth"
+        to="/"
+      >
+        Beranda
+      </router-link>
+    </template>
+  </app-content>
+</template>
+
+<script lang="ts" src="./about.component.ts"></script>
+<style lang="scss" scoped src="./about.component.scss"></style>
