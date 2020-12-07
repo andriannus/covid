ad66ace4df1173afb3db8d541ef1063b5524a8bd
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-07 19:27:16 +0700

    feat: create wrapper for general pages

---
+++ src/app/general/general.component.ts
@@ -0,0 +1,11 @@
+import { defineComponent } from '@vue/composition-api';
+
+import AppPage from '@/app/shared/components/page/page.component.vue';
+
+export default defineComponent({
+  name: 'General',
+
+  components: {
+    AppPage,
+  },
+});
---
+++ src/app/general/general.component.vue
@@ -0,0 +1,8 @@
+<template>
+  <app-page>
+    <router-view></router-view>
+  </app-page>
+</template>
+
+<script lang="ts" src="./general.component.ts"></script>
+<style lang="scss" scoped src="./general.component.scss"></style>
