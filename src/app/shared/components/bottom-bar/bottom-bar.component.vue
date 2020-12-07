c4cd50c3c1844bba4cb294b4144101f1468429d3
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-07 17:01:17 +0700

    feat: create bottom-bar component

---
+++ src/app/shared/components/bottom-bar/bottom-bar.component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="BottomBar">
+    <ul class="BottomBar-menu">
+      <li class="BottomBar-menuItem">
+        <router-link class="Flex-column" to="/">
+          <app-icon icon="home" size="lg"></app-icon>
+          <span>Home</span>
+        </router-link>
+      </li>
+
+      <li class="BottomBar-menuItem">
+        <router-link class="Flex-column" to="/about">
+          <app-icon icon="info-circle" size="lg"></app-icon>
+          <span>About</span>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" src="./bottom-bar.component.ts"></script>
+<style lang="scss" scoped src="./bottom-bar.component.scss"></style>
