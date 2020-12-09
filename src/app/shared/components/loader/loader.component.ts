c3c09028424fe47571b1ad9fb3e98607e9728323
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-09 10:58:53 +0700

    feat: create loader component

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,11 @@
+import { defineComponent } from '@vue/composition-api';
+
+import VirusLogo from '@/assets/images/svg/virus.svg';
+
+export default defineComponent({
+  name: 'Loader',
+
+  components: {
+    VirusLogo,
+  },
+});
---
+++ src/app/shared/components/loader/loader.component.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="Loader">
+      <virus-logo class="Loader-logo"></virus-logo>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./loader.component.ts"></script>
+<style lang="scss" scoped src="./loader.component.scss"></style>
