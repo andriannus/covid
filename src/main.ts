3fed5f651cc3cfaf2f49d66c4e761111f0920a6b
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-19 13:31:26 +0700

    refactor: remove vuex

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,10 @@ import router from '@/app/app.routes';
 
 import '@/plugins';
 import '@/register-service-worker';
-import store from '@/store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app');
