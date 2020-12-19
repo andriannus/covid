cf0ce9b828090493f2d9b5ad244cbbb5ecc048dd
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-19 23:36:26 +0700

    feat: create dialog component

---
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -0,0 +1,63 @@
+import {
+  defineComponent,
+  onUnmounted,
+  reactive,
+  watch,
+} from '@vue/composition-api';
+
+import { CLIP_CLASS } from './dialog.constant';
+import { DialogState } from './dialog.model';
+
+import AppFade from '@/app/shared/components/fade/fade.component.vue';
+
+export default defineComponent({
+  name: 'Dialog',
+
+  components: {
+    AppFade,
+  },
+
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  setup(props) {
+    const state = reactive<DialogState>({
+      html: document.querySelector('html'),
+    });
+
+    onUnmounted(() => {
+      removeClipClass();
+    });
+
+    watch(
+      () => props.value,
+      value => {
+        if (value) {
+          return addClipClass();
+        }
+
+        removeClipClass();
+      },
+    );
+
+    const hasClipClass = () => {
+      return state.html?.classList.contains(CLIP_CLASS);
+    };
+
+    const addClipClass = () => {
+      if (!hasClipClass()) {
+        state.html?.classList.add(CLIP_CLASS);
+      }
+    };
+
+    const removeClipClass = () => {
+      if (hasClipClass()) {
+        state.html?.classList.remove(CLIP_CLASS);
+      }
+    };
+  },
+});
---
+++ src/app/shared/components/dialog/dialog.component.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-fade>
+    <div v-if="value" class="Dialog">
+      <div class="Dialog-backdrop"></div>
+
+      <div class="Dialog-box">
+        <div class="Dialog-block">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </app-fade>
+</template>
+
+<script lang="ts" src="./dialog.component.ts"></script>
