402b681e14217fc088b8dc9627c187bf16bcdd24
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-19 23:36:54 +0700

    feat: create fade component

---
+++ src/app/shared/components/fade/fade.component.vue
@@ -0,0 +1,8 @@
+<template>
+  <transition name="Fade">
+    <slot></slot>
+  </transition>
+</template>
+
+<script lang="ts" src="./fade.component.ts"></script>
+<style lang="scss" scoped src="./fade.component.scss"></style>
