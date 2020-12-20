57f860c5a1b8b8dd42811207733639bd3c46c5e3
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-20 22:02:05 +0700

    feat: create change language button component

---
+++ src/app/shared/components/button-language/button-language.component.ts
@@ -0,0 +1,34 @@
+import { defineComponent, reactive } from '@vue/composition-api';
+import { useI18n } from 'vue-i18n-composable';
+
+import AppDialog from '@/app/shared/components/dialog/dialog.component.vue';
+
+export default defineComponent({
+  name: 'ButtonLanguage',
+
+  components: {
+    AppDialog,
+  },
+
+  setup() {
+    const state = reactive({
+      isDialogShown: false,
+      i18n: useI18n(),
+    });
+
+    const toggleDialog = () => {
+      state.isDialogShown = !state.isDialogShown;
+    };
+
+    const setLocale = (selectedLocale: string) => {
+      state.i18n.locale = selectedLocale;
+      toggleDialog();
+    };
+
+    return {
+      setLocale,
+      state,
+      toggleDialog,
+    };
+  },
+});
---
+++ src/app/shared/components/button-language/button-language.component.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button
+      class="ButtonLanguage Button Button--primary Button--outlined Button--small Transform-uppercase"
+      @click="toggleDialog"
+    >
+      {{ state.i18n.locale }}
+    </button>
+
+    <app-dialog v-model="state.isDialogShown">
+      <button
+        class="Button Button--primary Button--outlined Button--fullWidth MarginBottom"
+        @click="setLocale('id')"
+      >
+        Bahasa Indonesia
+      </button>
+
+      <button
+        class="Button Button--primary Button--outlined Button--fullWidth MarginBottom"
+        @click="setLocale('en')"
+      >
+        English
+      </button>
+
+      <button
+        class="Button Button--primary Button--fullWidth"
+        @click="toggleDialog"
+      >
+        Done
+      </button>
+    </app-dialog>
+  </div>
+</template>
+
+<script lang="ts" src="./button-language.component.ts"></script>
+<style lang="scss" scoped src="./button-language.component.scss"></style>
