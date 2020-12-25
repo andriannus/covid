ecbf8766bf9c203990e89923009e42614b09dab8
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-25 14:42:57 +0700

    chore: update vue config for PWA

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,19 @@ module.exports = {
     },
   },
 
-  chainWebpack: config => {
+  pwa: {
+    name: 'COVID19 Monitoring',
+    themeColor: '#ffffff',
+    manifestOptions: {
+      background_color: '#ffffff',
+      description: 'COVID19 Monitoring with Vue.js and TypeScript',
+      short_name: 'COVID19',
+    },
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
+
+  chainWebpack(config) {
     config.plugin('html').tap(options => {
       options[0].title = 'COVID19';
       return options;
