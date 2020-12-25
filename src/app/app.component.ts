f7b2af777f4cb491367277ef1baaec6beb7bc487
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-25 13:42:32 +0700

    chore: improve SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,5 +4,12 @@ export default defineComponent({
   metaInfo: {
     title: 'Selamat Datang!',
     titleTemplate: '%s — COVID19',
+    meta: [
+      {
+        vmid: 'description',
+        name: 'description',
+        content: 'COVID19 Monitoring with Vue.js and TypeScript',
+      },
+    ],
   },
 });
